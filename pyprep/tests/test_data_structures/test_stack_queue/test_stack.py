6fb96d7d4db3a7aa2842b025fe4d649111cb0dcc
hlatourette <hlatourette@gmail.com>
2018-05-20 23:36:18 -0500

    feat (sorting): implement quicksort
    quicksort unit tests
    
    17

--- pyprep/tests/test_data_structures/test_stack_queue/test_stack.py
+++ pyprep/tests/test_data_structures/test_stack_queue/test_stack.py
@@ -4,41 +4,41 @@ from data_structures.stack_queue.stack import Stack
 
 
 class TestStack(unittest.TestCase):
-  def setUp(self):
-      self.stack = Stack()
+    def setUp(self):
+        self.stack = Stack()
 
-  def test_push(self):
-      for i in range(0, 5):
-          self.stack.push(i)
-          self.assertEqual(self.stack.peek(), i)
+    def test_push(self):
+        for i in range(0, 5):
+            self.stack.push(i)
+            self.assertEqual(self.stack.peek(), i)
 
-  def test_pop(self):
-      inputs = range(0, 5)
-      for i in inputs:
-          self.stack.push(i)
+    def test_pop(self):
+        inputs = range(0, 5)
+        for i in inputs:
+            self.stack.push(i)
 
-      for i in reversed(inputs):
-          self.assertEqual(self.stack.pop(), i)
+        for i in reversed(inputs):
+            self.assertEqual(self.stack.pop(), i)
 
-  def test_pop_empty(self):
-      self.assertRaises(Exception, self.stack.pop)
+    def test_pop_empty(self):
+        self.assertRaises(Exception, self.stack.pop)
 
-  def test_pop_single(self):
-      self.stack.push(1)
-      self.assertEqual(self.stack.pop(), 1)
-      self.assertRaises(Exception, self.stack.pop)
+    def test_pop_single(self):
+        self.stack.push(1)
+        self.assertEqual(self.stack.pop(), 1)
+        self.assertRaises(Exception, self.stack.pop)
 
-  def test_peek(self):
-      for i in range(0, 5):
-          self.stack.push(i)
-          self.assertEqual(self.stack.peek(), i)
+    def test_peek(self):
+        for i in range(0, 5):
+            self.stack.push(i)
+            self.assertEqual(self.stack.peek(), i)
 
-  def test_peek_empty(self):
-      self.assertRaises(Exception, self.stack.peek)
+    def test_peek_empty(self):
+        self.assertRaises(Exception, self.stack.peek)
   
-  def test_is_empty_true(self):
-      self.assertEqual(self.stack.is_empty(), True)
+    def test_is_empty_true(self):
+        self.assertEqual(self.stack.is_empty(), True)
 
-  def test_is_empty_false(self):
-      self.stack.push(1)
-      self.assertEqual(self.stack.is_empty(), False)
+    def test_is_empty_false(self):
+        self.stack.push(1)
+        self.assertEqual(self.stack.is_empty(), False)
