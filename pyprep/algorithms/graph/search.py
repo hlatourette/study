0948099e627729ddba5af4bf8d474924b5fd3ea1
hlatourette <hlatourette@gmail.com>
2018-06-17 13:36:12 -0500

    feat (graph): implement dfs and bfs
    algos use a key for sorting to guarantee neighbor order
    allows for deterministic unit testing
    
    7

--- pyprep/algorithms/graph/search.py
+++ pyprep/algorithms/graph/search.py
@@ -1,6 +1,27 @@
-def dfs(graph, func):
-    pass
+from data_structures.stack_queue.queue import Queue
 
-def bfs(graph, func):
-    pass
-    
+
+def dfs(graph, vertex, func, neighbor_sort_key=lambda val: val):
+    visited = set()
+    _dfs(graph, vertex, visited, func, neighbor_sort_key)
+
+def _dfs(graph, vertex, visited, func, sort_key):
+    func(vertex)
+    visited.add(vertex)
+    for neighbor in sorted(graph[vertex], key=sort_key):
+        if neighbor not in visited:
+            _dfs(graph, neighbor, visited, func, sort_key)
+
+
+def bfs(graph, vertex, func, neighbor_sort_key=lambda val: val):
+    queue = Queue()
+    visited = set()
+    visited.add(vertex)
+    queue.enqueue(vertex)
+    while not queue.is_empty():
+        v = queue.dequeue()
+        func(v)
+        for neighbor in sorted(graph[v]):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.enqueue(neighbor)
