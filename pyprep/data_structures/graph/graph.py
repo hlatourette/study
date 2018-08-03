0559040e754085baf801e46d2b9c7160b870ae92
hlatourette <hlatourette@gmail.com>
2018-08-02 23:12:19 -0500

    feat (graph): add attributes to graph
    remove multigraph as it can be impl with attributes if necessary
    update tests to reflect internal structure
    
    19

--- pyprep/data_structures/graph/graph.py
+++ pyprep/data_structures/graph/graph.py
@@ -2,10 +2,9 @@ from collections import defaultdict
 
 
 class Graph:
-    def __init__(self, adj_list={}, directed=False, multigraph=False):
-        self._adj = defaultdict(list)
+    def __init__(self, adj_list={}, directed=False):
+        self._adj = defaultdict(dict)
         self._directed = directed
-        self._multigraph = multigraph
         self.add_edges(adj_list=adj_list)
 
     def __getitem__(self, vertex):
@@ -17,25 +16,22 @@ class Graph:
     def __delitem__(self, vertex):
         del self._adj[vertex]
         for u, adj in self._adj.items():
-            self._adj[u] = [val for val in adj if val != vertex]
+            if vertex in self._adj[u]:
+                del self._adj[u][vertex]
 
     def __len__(self):
         return len(self._adj)
 
-    def add_edges(self, adj_list={}, edge_list=[]):
+    def add_edges(self, adj_list={}):
         for u, adj in adj_list.items():
-            for v in adj:
-                self.add_edge(u, v)
+            for v, attr in adj.items():
+                self.add_edge(u, v, attr)
 
-        for edge in edge_list:
-            self.add_edge(edge[0], edge[1])
-
-    def add_edge(self, u_of_edge, v_of_edge):
-        if u_of_edge in self._adj and v_of_edge in self._adj[u_of_edge] and not self._multigraph:
+    def add_edge(self, u_of_edge, v_of_edge, attributes={}):
+        if u_of_edge in self._adj and v_of_edge in self._adj[u_of_edge]:
             raise Exception
 
-        self._adj[u_of_edge].append(v_of_edge)
+        self._adj[u_of_edge][v_of_edge] = attributes
         self._adj[v_of_edge]
         if not self._directed and u_of_edge != v_of_edge:
-            self._adj[v_of_edge].append(u_of_edge)
-            
+            self._adj[v_of_edge][u_of_edge] = attributes
